04813b85a951dda743cfed85d7f681adb2a56430
Bulkan Evcimen <bulkan@gmail.com>
2013-11-23 23:51:02 +1100

    query condition endpoint of wunderground
    the test at the moment does the http call. I’ll mock this out next

--- tests/weather_test.js
+++ tests/weather_test.js
@@ -1,13 +1,22 @@
 var should = require('chai').should()
   , sinon = require('sinon')
   , r = require('request')
+  , apiKey = require('../wunderground').key
   , Weather = require('../lib/weather');
-
 
 describe('Weather', function(){
   it('class instantiates correctly', function(done){
-    var w = new Weather('apikey');
+    var w = new Weather(apiKey);
     done();
+  });
+
+  it('can get the condition', function(done){
+    var w = new Weather(apiKey);
+    w.condition('Australia', 'Melbourne', function(err, conditionData){
+      if (err) return done(err);
+      console.log(conditionData);
+      done();
+    });
   });
 
 });
