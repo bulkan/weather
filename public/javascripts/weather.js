256e0c07a1f0acf98a59d5cf986037364d02788d
Bulkan Evcimen <bulkan@gmail.com>
2013-11-25 20:18:49 +1100

    fixed bug were status images werent being reset

--- public/javascripts/weather.js
+++ public/javascripts/weather.js
@@ -19,22 +19,31 @@ function autocomplete(json){
 
     results.slideUp().html('');
 
+    // read the location from the input box
     $('#location').val(location.text());
 
+    // remove the previous img tags that show the current weather
+    var weather = $('#weather');
+    weather.find('img').remove();
+    var current_temp = $('#current_temp');
+    current_temp.html('');
 
     $.ajax('/condition?link=' + location.attr('data-link') ).done(function(data){
-      var weather = $('#weather');
       var co = data.current_observation;
-      $('#current_temp').html(co.temperature_string);
+      // show the current weather string
+      current_temp.html(co.temperature_string);
       $('<img>').attr('src', co.icon_url).appendTo(weather);
+      weather.fadeIn();
     });
   });
 
 };
 
+// store the return value for the setTimeout below to be able to clear out later.
 var timer = void 0;
 
 $(document).ready(function(){
+   // seems like you dont need the api `key` for the AutoComplete api
    var url = 'http://autocomplete.wunderground.com/aq?format=JSON&query=';
 
    $('#location').bind('keyup', function(e) {
